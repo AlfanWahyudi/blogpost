1a2e76ea9bbc6efd28e6a9302d8c57f31711619d
Alfan Wahyudi <alfan.wahyudi98@gmail.com>
2023-07-19 22:50:59 +0700

    update sign up forms to add asterisk at form label

--- resources/views/pages/auth/register.blade.php
+++ resources/views/pages/auth/register.blade.php
@@ -7,19 +7,19 @@
         <h1 class="text-center mb-5 fs-3 fw-semibold">Create an account</h1>
         <form action="" class="d-flex flex-column justify-content-center mb-5">
             <div class="mb-3">
-                <label for="name" class="form-label">Name</label>
+                <label for="name" class="form-label">Name <span class="text-danger">*</span></label>
                 <input type="text" id="name" name="name" class="form-control form-control-lg fs-6" placeholder="Enter your full name" required>
             </div>
             <div class="mb-3">
-                <label for="email" class="form-label">Email</label>
+                <label for="email" class="form-label">Email <span class="text-danger">*</span></label>
                 <input type="email" id="email" name="email" class="form-control form-control-lg fs-6" placeholder="Enter your email" required>
             </div>
             <div class="mb-4">
-                <label for="password" class="form-label">Password</label>
+                <label for="password" class="form-label">Password <span class="text-danger">*</span></label>
                 <input type="password" id="password" name="password" class="form-control form-control-lg fs-6" placeholder="·········" required>
             </div>
             <div class="mb-4">
-                <label for="confirmPwd" class="form-label">Confirm Password</label>
+                <label for="confirmPwd" class="form-label">Confirm Password <span class="text-danger">*</span></label>
                 <input type="password" id="confirmPwd" name="confirm_password" class="form-control form-control-lg fs-6" placeholder="·········" required>
             </div>
             <button type="submit" class="btn btn-primary">Sign Up</button>
