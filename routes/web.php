7f5ec879ea2ec1f3a2bc9be89cab66d1f9c98955
Alfan Wahyudi <alfan.wahyudi98@gmail.com>
2023-07-30 20:33:00 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Dashboard\HomeDashboardController;
+use App\Http\Controllers\PostDashboardController;
 use App\Http\Controllers\Public\HomeController;
 use App\Http\Controllers\Public\PostController;
 use Illuminate\Support\Facades\Route;
@@ -19,11 +20,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+
+//TODO: change route /post/{slug} to /posts/{slug}
 Route::get('/post/{slug}', [PostController::class, 'show'])->name('post.detail');
 
 Route::middleware('guest')->group(function () {
     Route::controller(LoginController::class)->group(function () {
         Route::prefix('sign-in')->group(function () {
+
+            //TODO: change name of create route to signin.create
             Route::get('/', 'create')->name('signin');
             Route::post('/', 'authenticate')->name('signin.authenticate');
         });
@@ -41,7 +46,24 @@ Route::middleware('guest')->group(function () {
 Route::middleware('auth')->group(function () {
     Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
-    Route::prefix('dashboard')->group(function () {
-        Route::get('/', [HomeDashboardController::class, 'index'])->name('dashboard.home');
+    Route::prefix('dashboard')->name('dashboard.')->group(function () {
+        Route::get('/', [HomeDashboardController::class, 'index'])->name('home');
+
+        Route::controller(PostDashboardController::class)
+            ->prefix('posts')
+            ->name('post.')
+            ->group(function () {
+                Route::get('/', 'index')->name('index');
+
+                Route::get('/{slug}', 'show')->name('show');
+
+                Route::get('/create', 'create')->name('create');
+                Route::post('/create', 'store')->name('store');
+
+                Route::get('/edit/{slug}', 'edit')->name('edit');
+                Route::put('/edit/{slug}', 'update')->name('update');
+
+                Route::delete('/destroy/{post}', 'destroy')->name('destroy');
+            });
     });
 });
